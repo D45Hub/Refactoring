9692d56b315e26f122f035e738ec8f6c9a78d8f8
D45Hub <denis.thiessen@gmx.de>
2021-05-06 16:02:43 +0200

    Fixed logic mistake in tests.

--- refactoring/src/test/java/de/denisthiessen/refactoring/tests/CustomerTest.java
+++ refactoring/src/test/java/de/denisthiessen/refactoring/tests/CustomerTest.java
@@ -15,9 +15,9 @@ public class CustomerTest
     @Test
     public void testCustomerStatement()
     {
-        Movie movie1 = new Movie("Test Movie 1", 100);
-        Movie movie2 = new Movie("Test Movie 2", 200);
-        Movie movie3 = new Movie("Test Movie 3", 50);
+        Movie movie1 = new Movie("Test Movie 1", Movie.CHILDRENS);
+        Movie movie2 = new Movie("Test Movie 2", Movie.NEW_RELEASE);
+        Movie movie3 = new Movie("Test Movie 3", Movie.REGULAR);
 
         Rental rental1 = new Rental(movie1, 1);
         Rental rental2 = new Rental(movie2, 4);
@@ -29,16 +29,16 @@ public class CustomerTest
         testCustomer.addRental(rental3);
 
         assertEquals(
-                "Rental Record for TestCustomer\n\tTitle\t\tDays\tAmount\n\tTest Movie 1\t\t1\t0.0\n\tTest Movie 2\t\t4\t0.0\n\tTest Movie 3\t\t2\t0.0\nAmount owed is 0.0\nYou earned 3 frequent renter points",
+                "Rental Record for TestCustomer\n\tTitle\t\tDays\tAmount\n\tTest Movie 1\t\t1\t2.0\n\tTest Movie 2\t\t4\t5.0\n\tTest Movie 3\t\t2\t2.0\nAmount owed is 9.0\nYou earned 3 frequent renter points",
                 testCustomer.statement());
     }
 
     @Test
     public void testCustomerHTMLStatement()
     {
-        Movie movie1 = new Movie("Test Movie 1", 100);
-        Movie movie2 = new Movie("Test Movie 2", 200);
-        Movie movie3 = new Movie("Test Movie 3", 50);
+        Movie movie1 = new Movie("Test Movie 1", Movie.CHILDRENS);
+        Movie movie2 = new Movie("Test Movie 2", Movie.NEW_RELEASE);
+        Movie movie3 = new Movie("Test Movie 3", Movie.REGULAR);
 
         Rental rental1 = new Rental(movie1, 1);
         Rental rental2 = new Rental(movie2, 4);
@@ -50,7 +50,7 @@ public class CustomerTest
         testCustomer.addRental(rental3);
 
         assertEquals(
-                "<h1>Rentals for <em>TestCustomer</em></h1><p>\nTest Movie 1: 0.0<br>\nTest Movie 2: 0.0<br>\nTest Movie 3: 0.0<br>\n<p>You owe <em>0.0</em></p>\nOn this rental you earned <em>3</em> frequent renter points</p>",
+                "<h1>Rentals for <em>TestCustomer</em></h1><p>\nTest Movie 1: 2.0<br>\nTest Movie 2: 5.0<br>\nTest Movie 3: 2.0<br>\n<p>You owe <em>9.0</em></p>\nOn this rental you earned <em>3</em> frequent renter points</p>",
                 testCustomer.htmlStatement());
     }
 
