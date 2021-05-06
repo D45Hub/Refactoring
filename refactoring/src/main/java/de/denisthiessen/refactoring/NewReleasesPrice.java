392c6d23cf041b0057bdbfb8f65a2124ffdf25fe
D45Hub <denis.thiessen@gmx.de>
2021-05-06 16:35:48 +0200

    Moved charge calculations to individual classes.

--- refactoring/src/main/java/de/denisthiessen/refactoring/NewReleasesPrice.java
+++ refactoring/src/main/java/de/denisthiessen/refactoring/NewReleasesPrice.java
@@ -9,4 +9,10 @@ public class NewReleasesPrice extends Price
         return Movie.NEW_RELEASE;
     }
 
+    @Override
+    public double getCharge(int daysRented)
+    {
+        return daysRented * 3;
+    }
+
 }
